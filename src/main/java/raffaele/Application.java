f300f80aeadba6153913d9a3421812d0233a2ce5
raffaelebaiocco06-ux <raffaele.baiocco06@gmail.com>
2026-03-26 16:32:12 +0100

    concerti

--- src/main/java/raffaele/Application.java
+++ src/main/java/raffaele/Application.java
@@ -20,8 +20,12 @@ public class Application {
         EventoDao eventoDAO = new EventoDao(em);
 
         Concerto c1 = new Concerto("Rock in Roma", LocalDate.of(2026, 7, 10), "Concerto rock all'aperto", EventType.PUBBLICO, 5000, Genere.ROCK, true);
+        Concerto c2 = new Concerto("Notte Pop", LocalDate.of(2026, 8, 15), "Grande evento pop", EventType.PUBBLICO, 3000, Genere.POP, false);
 
-        eventoDAO.save(c1);
+        Concerto c3 = new Concerto("Mozart Night", LocalDate.of(2026, 9, 1), "Concerto di musica classica", EventType.PUBBLICO, 1200, Genere.CLASSICO, true);
+      //  eventoDAO.save(c1);
+       // eventoDAO.save(c2);
+      //  eventoDAO.save(c3);
 
 
         System.out.println("=== CONCERTI IN STREAMING ===");
